018b85e8db70466c33a3c5d1cbc2010c242652d8
Ubuntu <ubuntu@ip-172-31-95-156.ec2.internal>
2019-09-11 01:00:53 +0000

    modified output file

--- terraform-vpc/output.tf
+++ terraform-vpc/output.tf
@@ -7,3 +7,11 @@ output "this_pub_subnet" {
   description = "Public Subnet"
   value = aws_subnet.pub_subnet.id
 }
+output "public_instnace" {
+  value = ["${aws_instance.pub-ec2.id}", "${aws_instance.pub-ec2.private_ip}", "${aws_instance.pub-ec2.public_ip}"]
+}
+output "private_instance" {
+  value = ["${aws_instance.private-ec2.id}", "${aws_instance.private-ec2.private_ip}"]
+}
+
+
